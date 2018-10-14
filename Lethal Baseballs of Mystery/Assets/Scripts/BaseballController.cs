dd43d9dfa8e99d2b29fe2d6f1544face41435dc9
Vince Carpino <carpi111@mail.chapman.edu>
2018-10-13 20:33:57 -0700

    Add pitching and random good/bad baseballs

---
+++ Lethal Baseballs of Mystery/Assets/Scripts/BaseballController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BaseballController : MonoBehaviour {
+
+    public float Speed;
+
+	void Start () {
+        Destroy(gameObject, 3f);
+	}
+	
+	void Update () {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+	}
+}
---
+++ Lethal Baseballs of Mystery/Assets/Scripts/PitcherController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PitcherController : MonoBehaviour {
+
+    public BaseballController Baseball;
+    public Transform PitchPoint;
+    public Material GoodBallMaterial;
+    public Material BadBallMaterial;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            var newBaseball = Instantiate(Baseball, PitchPoint.position, PitchPoint.rotation);
+            int random = Random.Range(0, 100);
+            newBaseball.GetComponent<MeshRenderer>().material = random % 2 == 0 ? GoodBallMaterial : BadBallMaterial;
+        }
+	}
+}
